49864eaac4b3e81db5f1d33f526fa7cbcb7f83bc
viju-dev <vijaydorugade20@gmail.com>
2023-07-22 23:54:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from "./Components/Navbar";
+import BookList from "./Components/BookList";
+import BookDetails from "./Components/BookDetails";
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 function App() {
+
+  const [books,setBooks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+   useEffect(()=>{
+    const handleSearch = async (queryyy) => {
+       
+       
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/books/v1/volumes?q=${queryyy}`
+        );
+  
+        const searchResult = response.data.items;
+        
+        setBooks(searchResult);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+      }
+    };
+    console.log(searchQuery,"showww")
+    handleSearch({searchQuery})
+  },[searchQuery])
+  
+
+  useEffect(()=>{
+    
+    const DataFetch = async ()=>{
+
+      try{
+        const resultsHerry = await axios.get("https://www.googleapis.com/books/v1/volumes?q=harry+potter");
+        const resultsSharelock = await axios.get("https://www.googleapis.com/books/v1/volumes?q=Sherlock+Holmes"); 
+        const AllData = resultsHerry.data.items.concat(resultsSharelock.data.items)
+        // console.log(AllData)
+        setBooks(()=>AllData);
+      }
+      catch(error){console.log(error,"error Fetching time.....")}
+    }
+    DataFetch()
+  },[]);
+  
+  // console.log(books,"bokesdfas",books.length>1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      <Navbar onSearch={setSearchQuery}/>
+      {books.length>0&&
+        <BookList books={books}/>
+      }
     </div>
   );
 }
